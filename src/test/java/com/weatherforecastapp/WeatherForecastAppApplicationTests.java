d9a5af8ed2d390f82b17be01511bd3095c1faa61
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2023-08-03 11:11:44 +0200

    V1

---
+++ src/test/java/com/weatherforecastapp/WeatherForecastAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherforecastapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
